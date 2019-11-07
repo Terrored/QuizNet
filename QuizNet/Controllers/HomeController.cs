8e36cf7c8e327eb6f0d6083a051f328b9805338b
Daniel Wilkiewicz <danielwilkiewicz@gmail.com>
2019-11-07 22:28:51 +0100

    Workshops #1 - Creating project.

---
+++ QuizNet/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using QuizNet.Models;
+
+namespace QuizNet.Controllers
+{
+    public class HomeController : Controller
+    {
+        public static List<Student> students = new List<Student>()
+        {
+            new Student()
+            {
+                Id=1,
+                Index=123456,
+                FirstName = "Daniel",
+                LastName = "Wilkiewicz"
+
+            },
+            new Student()
+            {
+                Id=2,
+                Index=35453,
+                FirstName = "Krzysztof",
+                LastName = "Milewski"
+
+            },
+    };
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(int id)
+        {
+            var student = students.FirstOrDefault(s => s.Id == id);
+            return View(student);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ QuizNet/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizNet.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Index { get; set; }
+
+    }
+}

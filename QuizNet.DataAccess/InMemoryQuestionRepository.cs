337b09d6c8c214d32bdd00041ec1e32d9e763831
KrzysztofMilewski <42148814+KrzysztofMilewski@users.noreply.github.com>
2019-11-26 19:41:16 +0100

    Workshops 3 homework (#4)
    * Homework - Update method + simple refactor
    
    * Adding more questions
    
    * Bug fixes

--- QuizNet.DataAccess/InMemoryQuestionRepository.cs
+++ QuizNet.DataAccess/InMemoryQuestionRepository.cs
@@ -1,8 +1,6 @@
-﻿using System;
+﻿using QuizNet.DataAccess.Models;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using QuizNet.DataAccess.Models;
 
 namespace QuizNet.DataAccess
 {
@@ -49,41 +47,140 @@ namespace QuizNet.DataAccess
             },
             new Question()
             {
-            Id = 2,
-            Text = "Jaki jest najlepszy język programowania?",
-            CorrectAnswerIndex = 2,
-            Answers = new Answer[]
+                Id = 2,
+                Text = "Jaki jest najlepszy język programowania?",
+                CorrectAnswerIndex = 2,
+                Answers = new Answer[]
+                {
+                    new Answer()
+                    {
+                        Id = 5,
+                        QuestionId = 2,
+                        Text = "HTML"
+
+                    },
+                    new Answer()
+                    {
+                        Id = 6,
+                        QuestionId = 2,
+                        Text = "Java"
+
+                    },
+                    new Answer()
+                    {
+                        Id = 7,
+                        QuestionId = 2,
+                        Text = "C#"
+
+                    },
+                    new Answer()
+                    {
+                        Id = 8,
+                        QuestionId = 2,
+                        Text = "JavaScript"
+
+                    },
+                }
+            },
+            new Question()
             {
-                new Answer()
+                Id = 3,
+                Text = "Jakiej metody HTTP używamy aby pobrać dane z serwera?",
+                CorrectAnswerIndex = 3,
+                Answers = new Answer[]
                 {
-                    Id = 5,
-                    QuestionId = 2,
-                    Text = "HTML"
-
-                },
-                new Answer()
+                    new Answer()
+                    {
+                        Id = 9,
+                        QuestionId = 3,
+                        Text = "POST"
+                    },
+                    new Answer()
+                    {
+                        Id = 10,
+                        QuestionId = 3,
+                        Text = "PUT"
+                    },
+                    new Answer()
+                    {
+                        Id = 11,
+                        QuestionId = 3,
+                        Text = "DELETE"
+                    },
+                    new Answer()
+                    {
+                        Id = 12,
+                        QuestionId = 3,
+                        Text = "GET"
+                    },
+                }
+            },
+            new Question()
+            {
+                Id = 4,
+                Text = "Na jakim systemie operacyjnym możemy uruchomić aplikację napisaną w .NET Core?",
+                CorrectAnswerIndex = 3,
+                Answers = new Answer[]
                 {
-                    Id = 6,
-                    QuestionId = 2,
-                    Text = "Java"
-
-                },
-                new Answer()
+                    new Answer()
+                    {
+                        Id = 13,
+                        QuestionId = 4,
+                        Text = "Windows"
+                    },
+                    new Answer()
+                    {
+                        Id = 14,
+                        QuestionId = 4,
+                        Text = "Linux"
+                    },
+                    new Answer()
+                    {
+                        Id = 15,
+                        QuestionId = 4,
+                        Text = "Mac OS"
+                    },
+                    new Answer()
+                    {
+                        Id = 16,
+                        QuestionId = 4,
+                        Text = "Wszystkie wymienione"
+                    },
+                }
+            },
+            new Question()
+            {
+                Id = 5,
+                Text = "Które zdanie najlepiej opisuje kontroler?",
+                CorrectAnswerIndex = 2,
+                Answers = new Answer[]
                 {
-                    Id = 7,
-                    QuestionId = 2,
-                    Text = "C#"
-
-                },
-                new Answer()
-                {
-                    Id = 8,
-                    QuestionId = 2,
-                    Text = "JavaScript"
-
-                },
+                    new Answer()
+                    {
+                        Id = 17,
+                        QuestionId = 5,
+                        Text = "Wykonuje operacje przeszukiwania i modyfikacji bazy danych"
+                    },
+                    new Answer()
+                    {
+                        Id = 18,
+                        QuestionId = 5,
+                        Text = "Realizuje algorytmy sztucznej inteligencji"
+                    },
+                    new Answer()
+                    {
+                        Id = 19,
+                        QuestionId = 5,
+                        Text = "Odpowiada na zapytania wysyłane przez klienta"
+                    },
+                    new Answer()
+                    {
+                        Id = 20,
+                        QuestionId = 5,
+                        Text = "Tworzy instancje obiektów zainicjalizowane w kontenerze DI"
+                    },
+                }
             }
-        }
         };
 
         public IEnumerable<Question> GetAll()
@@ -98,12 +195,32 @@ namespace QuizNet.DataAccess
 
         public void Add(Question question)
         {
+            var newQuestionId = _questions.Last().Id + 1;
+            question.Id = newQuestionId;
+
+            var lastAnswerId = _questions.LastOrDefault().
+                Answers.LastOrDefault().Id;
+
+
+            for (int i = 0; i < question.Answers.Length; i++)
+            {
+                question.Answers[i].Id = lastAnswerId + i + 1;
+                question.Answers[i].QuestionId = newQuestionId;
+            }
+
             _questions.Add(question);
         }
 
         public void Update(Question updatedQuestion)
         {
-            throw new NotImplementedException();
+            var questionToEdit = _questions.FirstOrDefault(q => q.Id == updatedQuestion.Id);
+            questionToEdit.Text = updatedQuestion.Text;
+            questionToEdit.CorrectAnswerIndex = updatedQuestion.CorrectAnswerIndex;
+
+            for (int i = 0; i < updatedQuestion.Answers.Length; i++)
+            {
+                questionToEdit.Answers[i].Text = updatedQuestion.Answers[i].Text;
+            }
         }
 
         public void Delete(int questionId)
--- QuizNet/Controllers/HomeController.cs
+++ QuizNet/Controllers/HomeController.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using QuizNet.DataAccess.Models;
 using QuizNet.Models;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 
 namespace QuizNet.Controllers
 {

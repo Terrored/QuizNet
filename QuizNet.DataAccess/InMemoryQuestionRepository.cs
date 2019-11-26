b25ef2fde5b4abfc7c85abf273d72ae28576e7dc
Krzysztof <milewskik51@gmail.com>
2019-11-26 19:08:22 +0100

    Adding more questions

--- QuizNet.DataAccess/InMemoryQuestionRepository.cs
+++ QuizNet.DataAccess/InMemoryQuestionRepository.cs
@@ -47,41 +47,140 @@ namespace QuizNet.DataAccess
             },
             new Question()
             {
-            Id = 2,
-            Text = "Jaki jest najlepszy język programowania?",
-            CorrectAnswerIndex = 2,
-            Answers = new Answer[]
+                Id = 2,
+                Text = "Jaki jest najlepszy język programowania?",
+                CorrectAnswerIndex = 2,
+                Answers = new Answer[]
+                {
+                    new Answer()
+                    {
+                        Id = 5,
+                        QuestionId = 2,
+                        Text = "HTML"
+
+                    },
+                    new Answer()
+                    {
+                        Id = 6,
+                        QuestionId = 2,
+                        Text = "Java"
+
+                    },
+                    new Answer()
+                    {
+                        Id = 7,
+                        QuestionId = 2,
+                        Text = "C#"
+
+                    },
+                    new Answer()
+                    {
+                        Id = 8,
+                        QuestionId = 2,
+                        Text = "JavaScript"
+
+                    },
+                }
+            },
+            new Question()
             {
-                new Answer()
+                Id = 3,
+                Text = "Jakiej metody HTTP używamy aby pobrać dane z serwera?",
+                CorrectAnswerIndex = 3,
+                Answers = new Answer[]
                 {
-                    Id = 5,
-                    QuestionId = 2,
-                    Text = "HTML"
-
-                },
-                new Answer()
+                    new Answer()
+                    {
+                        Id = 9,
+                        QuestionId = 3,
+                        Text = "POST"
+                    },
+                    new Answer()
+                    {
+                        Id = 10,
+                        QuestionId = 3,
+                        Text = "PUT"
+                    },
+                    new Answer()
+                    {
+                        Id = 11,
+                        QuestionId = 3,
+                        Text = "DELETE"
+                    },
+                    new Answer()
+                    {
+                        Id = 12,
+                        QuestionId = 3,
+                        Text = "GET"
+                    },
+                }
+            },
+            new Question()
+            {
+                Id = 4,
+                Text = "Na jakim systemie operacyjnym możemy uruchomić aplikację napisaną w .NET Core?",
+                CorrectAnswerIndex = 3,
+                Answers = new Answer[]
                 {
-                    Id = 6,
-                    QuestionId = 2,
-                    Text = "Java"
-
-                },
-                new Answer()
+                    new Answer()
+                    {
+                        Id = 13,
+                        QuestionId = 4,
+                        Text = "Windows"
+                    },
+                    new Answer()
+                    {
+                        Id = 14,
+                        QuestionId = 4,
+                        Text = "Linux"
+                    },
+                    new Answer()
+                    {
+                        Id = 15,
+                        QuestionId = 4,
+                        Text = "Mac OS"
+                    },
+                    new Answer()
+                    {
+                        Id = 16,
+                        QuestionId = 4,
+                        Text = "Wszystkie wymienione"
+                    },
+                }
+            },
+            new Question()
+            {
+                Id = 5,
+                Text = "Które zdanie najlepiej opisuje kontroler?",
+                CorrectAnswerIndex = 2,
+                Answers = new Answer[]
                 {
-                    Id = 7,
-                    QuestionId = 2,
-                    Text = "C#"
-
-                },
-                new Answer()
-                {
-                    Id = 8,
-                    QuestionId = 2,
-                    Text = "JavaScript"
-
-                },
+                    new Answer()
+                    {
+                        Id = 17,
+                        QuestionId = 5,
+                        Text = "Wykonuje operacje przeszukiwania i modyfikacji bazy danych"
+                    },
+                    new Answer()
+                    {
+                        Id = 18,
+                        QuestionId = 5,
+                        Text = "Realizuje algorytmy sztucznej inteligencji"
+                    },
+                    new Answer()
+                    {
+                        Id = 19,
+                        QuestionId = 5,
+                        Text = "Odpowiada na zapytania wysyłane przez klienta"
+                    },
+                    new Answer()
+                    {
+                        Id = 20,
+                        QuestionId = 5,
+                        Text = "Tworzy instancje obiektów zainicjalizowane w kontenerze DI"
+                    },
+                }
             }
-        }
         };
 
         public IEnumerable<Question> GetAll()

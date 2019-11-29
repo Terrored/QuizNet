7723a09536c4064231ba87113a4f8ba92c6a2e6f
Daniel Wilkiewicz <danielwilkiewicz@gmail.com>
2019-11-29 16:59:24 +0100

    Workshops4 Introducing Service, Dto, ViewModel, Forms Validation

---
+++ QuizNet.BusinessLogic/DTOs/QuestionDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using QuizNet.DataAccess.Models;
+
+namespace QuizNet.BusinessLogic.DTOs
+{
+    public class QuestionDto
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Please specify question text")]
+        [StringLength(5)]
+        public string Text { get; set; }
+        public AnswerDto[] Answers { get; set; }
+        public int CorrectAnswerIndex { get; set; }
+    }
+}
---
+++ QuizNet.BusinessLogic/QuizService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using QuizNet.BusinessLogic.DTOs;
+using QuizNet.BusinessLogic.Interfaces;
+using QuizNet.DataAccess;
+using QuizNet.DataAccess.Models;
+
+namespace QuizNet.BusinessLogic
+{
+    public class QuizService : IQuizService
+    {
+        private readonly IQuestionRepository _questionRepository;
+
+        public QuizService(IQuestionRepository questionRepository)
+        {
+            _questionRepository = questionRepository;
+        }
+        public List<QuestionDto> GenerateQuiz()
+        {
+            List<Question> questions = _questionRepository.GetAll().ToList();
+            List<Question> randomQuestions = questions.OrderBy(x => Guid.NewGuid()).Take(2).ToList();
+
+            List<QuestionDto> randomQuestionsDto = randomQuestions.Select(x => new QuestionDto()
+            {
+                Id = x.Id,
+                CorrectAnswerIndex = x.CorrectAnswerIndex,
+                Text = x.Text,
+                Answers = x.Answers.Select(y => new AnswerDto() {Id = y.Id, QuestionId = y.QuestionId, Text = y.Text}).ToArray()
+            }).ToList();
+            
+            return randomQuestionsDto;
+        }
+    }
+}
